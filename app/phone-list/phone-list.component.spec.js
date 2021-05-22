2035af1bce9e49fb4d63d5fa5f81864176c561da
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-05-22 18:22:14 +0100

    edited files

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -1,20 +1,30 @@
-describe("phoneList", function () {
-  // Load the module that contains the `phoneList` component before each test
-  beforeEach(module("phonecatApp"));
+describe('phoneList', function() {
 
-  // Test the controller
-  describe("PhoneListController", function () {
-    it("should create a `phones` model with 3 phones", inject(function (
-      $componentController
-    ) {
-      var ctrl = $componentController("phoneList");
+  beforeEach(module('phoneList'));
 
-      expect(ctrl.phones.length).toBe(3);
+  describe('controller', function() {
+    var $httpBackend, ctrl;
+
+    // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+    // This allows us to inject a service and assign it to a variable with the same name
+    // as the service while avoiding a name conflict.
+    beforeEach(inject(function($componentController, _$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/phones.json')
+                  .respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+
+      ctrl = $componentController('phoneList');
     }));
-  });
-});
 
-describe("phoneList", function () {
-  // Load the module that contains the `phoneList` component before each test
-  beforeEach(module("phoneList"));
-});
+    it('should create a `phones` property with 2 phones fetched with `$http`', function() {
+      expect(ctrl.phones).toBeUndefined();
+    
+      $httpBackend.flush();
+      expect(ctrl.phones).toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+    });
+
+    it('should set a default value for the `orderProp` property', function() {
+      expect(ctrl.orderProp).toBe('age');
+    });
+
+
