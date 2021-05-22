421ead2ea468ed382b61a33201cbc9c304cd80c7
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-05-22 18:14:58 +0100

    edited files

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,34 +1,24 @@
-// Register `phoneList` component, along with its associated controller and template
-angular.module("phonecatApp").component("phoneList", {
-  // This name is what AngularJS uses to match to the `<phone-list>` element.
-  template:
-    "<ul>" +
-    '<li ng-repeat="phone in $ctrl.phones">' +
-    "<span>{{phone.name}}</span>" +
-    "<p>{{phone.snippet}}</p>" +
-    "</li>" +
-    "</ul>",
+angular.module("phoneList").component("phoneList", {
+  templateUrl: "phone-list/phone-list.template.html",
   controller: function PhoneListController() {
     this.phones = [
       {
         name: "Nexus S",
         snippet: "Fast just got faster with Nexus S.",
+        age: 1,
       },
       {
         name: "Motorola XOOM™ with Wi-Fi",
         snippet: "The Next, Next Generation tablet.",
+        age: 2,
       },
       {
         name: "MOTOROLA XOOM™",
         snippet: "The Next, Next Generation tablet.",
+        age: 3,
       },
     ];
+
+    this.orderProp = "age";
   },
-});
-angular.
-module('phoneList').
-component('phoneList', {
-  // Note: The URL is relative to our `index.html` file
-  templateUrl: 'phone-list/phone-list.template.html',
-  controller: 
 });
